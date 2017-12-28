a01818b874443bfb84c98bf5330879daefa92c82
Stacey Watro <staceywatro@Staceys-MacBook-Pro.local>
2017-12-28 11:24:21 -0500

    Adds hello world lambda file.

---
+++ lambda.py
@@ -0,0 +1,10 @@
+import os
+
+
+def lambda_handler(event, context):
+    what_to_print = "hello world"
+    how_many_times = 5
+
+    for i in range(0, how_many_times):
+        print(f"what_to_print: {what_to_print}.")
+    return what_to_print

b0ac039c6ebc76229750adb45af92b1df5ce3240
Stacey Watro <staceywatro@Staceys-MacBook-Pro.local>
2017-12-28 13:12:21 -0500

    Testing that deployment works.

--- lambda.py
+++ lambda.py
@@ -5,6 +5,7 @@ def lambda_handler(event, context):
     what_to_print = "hello world"
     how_many_times = 5
 
+    print(f"Stacey is awesome!.")
     for i in range(0, how_many_times):
         print(f"what_to_print: {what_to_print}.")
     return what_to_print
